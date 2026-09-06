3879b2ba3ccb19443fb4aba4f5a4e503e0b35819
ambuj <srivastavvaambuj42@gmail.com>
2026-09-06 17:47:18 +0530

    updating service name

--- src/app/core/api-config.ts
+++ src/app/core/api-config.ts
@@ -1,3 +1,3 @@
 // Local default. For Vercel deployment, replace this with your Render JSON Server URL.
 // Example: https://home-expense-mock-api.onrender.com
-export const API_BASE_URL = 'http://localhost:3000';
+export const API_BASE_URL = 'home-expense-notebook-angular22.onrender.com';
